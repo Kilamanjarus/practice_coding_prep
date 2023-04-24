dbb8073a646776a9890d1bda574662d30da315c7
max <max.lillie95@gmail.com>
2023-04-24 12:22:51 -0500

    init, added questions for tech prep

---
+++ dev_cara_ruby_int_prep.rb
@@ -0,0 +1,19 @@
+# Write a function that takes an array of integers and returns the sum of the even numbers in the array.
+
+# Write a function that takes a string as input and returns the same string with the words in reverse order.
+
+# Write a function that takes a string and returns true if the string is a palindrome, and false otherwise.
+
+# Write a function that takes a string and returns the number of vowels in the string.
+
+# Write a function that takes an array of integers and returns the second largest number in the array.
+
+# Write a function that takes two arrays and returns a new array that contains the intersection of the two arrays (i.e., the elements that are common to both arrays).
+
+# Write a function that takes a string as input and returns a new string where each word is capitalized.
+
+# Write a function that takes a string as input and returns a new string where the first letter of each word is capitalized.
+
+# Write a function that takes an array of integers and returns a new array where each element is multiplied by 2.
+
+# Write a function that takes a string as input and returns the most common letter in the string.
